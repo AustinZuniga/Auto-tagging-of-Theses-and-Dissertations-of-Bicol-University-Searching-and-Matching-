8d4cc0f9af6e81af7e54fee73ccc711d9950d816
AustinZuniga <earlaustinavilazuniga@gmail.com>
2018-07-26 20:04:27 +0800

    added startup files

---
+++ test.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+from rake_nltk import Rake
+from nltk.corpus import stopwords 
+r = Rake() 
+
+f=open("data/data4.txt", "r")
+if f.mode == 'r':
+	contents =f.read()
+
+
+a=r.extract_keywords_from_text(contents)
+b=r.get_ranked_phrases()
+c=r.get_ranked_phrases_with_scores()
+print(b)
+print("\n\n\n\n")
+print(c[0][1])
